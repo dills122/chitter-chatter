8c0c8a7aa4c6a2aa395f6beb2c8903c25ece32c8
Dylan Steele <dylansteele57@gmail.com>
2019-02-02 20:45:59 -0500

    basic socket io setup

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-var app = express();
-var server = app.listen(4000, () => {
-    console.log('listening to requests on port 4000');
-});
-
-app.use(express.static('.\\src\\public'));
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(4000, () => {
+    console.log('listening to requests on port 4000');
+});
+
+app.use(express.static('.\\src\\public'));
+
+//Socket Setup
+
+const io = socket(server);
+
+
+io.on('connection', (socket) => {
+    console.log('socket running on new connection');
+    console.log(`made socket connection ${socket.id}`);
+});
+
