3f48f7cc64ebe74647440bec0aa19ea0f8b177dc
Dylan Steele <dylansteele57@gmail.com>
2019-02-02 21:39:55 -0500

    broadcast typing message and empty message warning

--- src/public/client/chat.js
+++ src/public/client/chat.js
@@ -4,7 +4,8 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
-
+var feedback = document.getElementById('feedback');
+var warning = document.getElementById('warning');
 
 btn.addEventListener('click', (event) => {
     socket.emit('chat', {
@@ -13,8 +14,36 @@ btn.addEventListener('click', (event) => {
     });
 });
 
+message.addEventListener('keypress', (event) => {
+    socket.emit('typing', handle.value);
+});
+
 
 
 socket.on('chat', (data) => {
-    output.innerHTML += `<p><strong>${data.handle}:</strong> ${data.message} </p>`;
+    feedback.innerHTML = '';
+    if(data && data.message !== '') {
+        output.innerHTML += `<p><strong>${data.handle}:</strong> ${data.message} </p>`;
+    } else {
+        EmptyMessage();
+    }
 });
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = `<p><em>${data} is typing...</em></p>`;
+    let timer = setTimeout(() => {
+        feedback.innerHTML = '';
+        clearTimeout(this);
+    }, 2500);
+});
+
+
+function EmptyMessage() {
+    warning.innerHTML = `<p><strong>Try typing something first before sending</strong></p>`;
+    let timer = setTimeout(() => {
+        warning.innerHTML = '';
+        clearTimeout(this);
+    }, 3000);    
+}
+
+
